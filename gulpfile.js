be0817ae2b75cac7e092063ed11d4451047e3605
czar.gaba <czargabadeveloper@gmail.com>
2024-09-23 20:19:48 +0800

    finalized node-sass concat compile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
 const gulp = require('gulp');
 const sftp = require('gulp-sftp-up4');
 const fs = require('fs');
+const sass = require('gulp-sass')(require('node-sass'));
+const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
+
 const config = JSON.parse(fs.readFileSync('staging.json', 'utf8'));
+
+// Define the SCSS compile and minify task
+gulp.task('scss', function () {
+    return gulp.src('asset/scss/*.scss') // specify the SCSS files to compile
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('style.css')) // concatenate into a single file
+        .pipe(cleanCSS({ compatibility: 'ie8' })) // minify the CSS
+        .pipe(gulp.dest('asset/css')); // output to the specified directory
+});
+
 // Define the deploy task
 gulp.task('deploy', function () {
-    return gulp.src('test/**/*.*')  // specify the files to upload
+    return gulp.src('test/**/*.*') // specify the files to upload
         .pipe(sftp({
             host: config.host,
             user: config.user,
@@ -15,9 +29,10 @@ gulp.task('deploy', function () {
 
 // Define the watch task
 gulp.task('watch', function () {
-    // Watch for changes in the /test directory
+    // Watch for changes in the /test directory and SCSS files
     gulp.watch('test/**/*.*', gulp.series('deploy'));
+    gulp.watch('asset/scss/*.scss', gulp.series('scss'));
 });
 
 // Default task to start watching
-gulp.task('default', gulp.series('watch'));
+gulp.task('default', gulp.series('scss', 'watch'));
