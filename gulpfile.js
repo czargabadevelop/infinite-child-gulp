9bb0066f0b98fc198eddc3d88a6546863b451747
SCDWD <saicrizofficial@gmail.com>
2025-01-11 11:05:30 +0800

    Stable gulpfile and update packages.

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,38 @@
 const gulp = require('gulp');
-const sftp = require('gulp-sftp-up4');
-const fs = require('fs');
-const sass = require('gulp-sass')(require('node-sass'));
+const sass = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const cleanCSS = require('gulp-clean-css');
+const sftp = require('gulp-sftp-up4');
+const fs = require('fs');
 
 const config = JSON.parse(fs.readFileSync('staging.json', 'utf8'));
-//"remotePath": "/path/to/remote/directory"
-///var/www/html/[clientfolder]/wp-content/themes/infinite-child -> this is the base in staging.json
 
-// Define the SCSS compile and minify task
+// Compile SCSS to CSS
 gulp.task('scss', function () {
-    return gulp.src('assets/scss/*.scss') // specify the SCSS files to compile
+    return gulp.src('assets/scss/**/*.scss') // Include all SCSS files
         .pipe(sass().on('error', sass.logError))
-        .pipe(concat('style.css')) // concatenate into a single file
-        .pipe(cleanCSS({ compatibility: 'ie8' })) // minify the CSS
-        .pipe(gulp.dest('assets/css')); // output to the specified directory
+        .pipe(concat('style.css')) // Combine into a single file
+        .pipe(cleanCSS({ compatibility: 'ie8' })) // Minify CSS
+        .pipe(gulp.dest('assets/css')) // Output directory
+        .on('end', () => console.log('SCSS compiled successfully.'));
 });
-//const config = JSON.parse(fs.readFileSync('staging.json', 'utf8'));
-// Define the deploy task
+
+// Deploy compiled assets
 gulp.task('deploy', function () {
-    return gulp.src('assets/**/*.*') // specify the files to upload
+    return gulp.src('assets/**/*.*') // Specify the files to upload
         .pipe(sftp({
             host: config.host,
             user: config.user,
             pass: config.pass,
-            remotePath: `${config.remotePath}/assets` //asset folder here
+            remotePath: `${config.remotePath}/assets`
         }));
 });
 
-// Define the watch task
+// Watch for changes in SCSS and deploy assets
 gulp.task('watch', function () {
-    // Watch for changes in the /test directory and SCSS files
-    gulp.watch('assets/scss/*.scss', gulp.series('scss'));
-    gulp.watch('assets/**/*.*', gulp.series('deploy'));
-    
+    gulp.watch('assets/scss/**/*.scss', gulp.series('scss')); // Watch all SCSS files
+    gulp.watch('assets/**/*.*', gulp.series('deploy')); // Watch all assets
 });
 
-// Default task to start watching
-gulp.task('default', gulp.series('scss','deploy', 'watch'));
+// Default task
+gulp.task('default', gulp.series('scss', 'deploy', 'watch'));
